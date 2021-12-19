3ff89a31375771d428374be58d4ff100e915b4f8
Erich L Foster <erichlf@gmail.com>
2021-12-19 11:13:57 -0500

    Do Some Checks Before Modifying Files

--- hid/enableModules.sh
+++ hid/enableModules.sh
@@ -3,10 +3,24 @@
 set -e
 # This was stolen from https://randomnerdtutorials.com/raspberry-pi-zero-usb-keyboard-hid/
 
-# back up files before we change anything
-sudo cp /boot/config.txt /boot/config.txt.bak
-sudo cp /etc/modules /etc/modules.bak
+# check to see if anything needs to be done
+isInFile=$(grep "dtoverlay=dwc2" /boot/config.txt)
+if [ $isInFile -eq 1 ]; then
+  # back up files before we change anything
+  sudo cp /boot/config.txt /boot/config.txt.bak
+  echo "dtoverlay=dwc2" | sudo tee -a /boot/config.txt
+fi
 
-echo "dtoverlay=dwc2" | sudo tee -a /boot/config.txt
-echo "dwc2" | sudo tee -a /etc/modules
-sudo echo "libcomposite" | sudo tee -a /etc/modules
+isInFile=$(grep "dwc2\|libcomposite" /etc/modules )
+if [ $isInFile -eq 1 ]; then
+  sudo cp /etc/modules /etc/modules.bak
+fi
+
+isInFile=$(grep "dwc2" /etc/modules )
+if [ $isInFile -eq 1 ]; then
+  echo "dwc2" | sudo tee -a /etc/modules
+fi
+isInFile=$(grep "libcomposite" /etc/modules )
+if [ $isInFile -eq 1 ]; then
+  sudo echo "libcomposite" | sudo tee -a /etc/modules
+fi
--- hid/install.sh
+++ hid/install.sh
@@ -10,5 +10,8 @@ sudo cp hemiola_usb /usr/bin/hemiola_usb
 sudo chmod +x /usr/bin/hemiola_usb
 
 # now have the hid device start on boot
-sudo cp /etc/rc.local /etc/rc.local.bak
-sudo sed -i "s/exit 0/\/usr\/bin\/hemiola_usb\nexit 0/" /etc/rc.local
+isInFile=$(grep "hemiola_usb" /etc/rc.local )
+if [ $isInFile -eq 1 ]; then
+  sudo cp /etc/rc.local /etc/rc.local.bak
+  sudo sed -i "s/exit 0/\/usr\/bin\/hemiola_usb\nexit 0/" /etc/rc.local
+fi
