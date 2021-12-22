c288f3956e36d1a024e2765e854639f6f0ea9c55
Erich L Foster <erichlf@gmail.com>
2021-12-22 09:06:30 -0500

    Add Exception Check to KeyboardEventTest

--- tests/KeyboardEventTest.cpp
+++ tests/KeyboardEventTest.cpp
@@ -1,7 +1,8 @@
 #include <gtest/gtest.h>
 
-#include "KeyboardEvents.h"
+#include "Exceptions.h"
 #include "FakeDevice.h"
+#include "KeyboardEvents.h"
 
 #include <exception>
 #include <iostream>
@@ -24,15 +25,27 @@ TEST ( KeyboardEventTest, KeyPressTest )
 
     // data that will be captured by keys
     std::vector<std::variant<wchar_t, unsigned short>> receivedData;
-    auto onEvent = [&receivedData]( std::variant<wchar_t, unsigned short> event ) {
+    auto onEvent = [&receivedData] ( std::variant<wchar_t, unsigned short> event ) {
         receivedData.push_back ( event );
     };
 
     // the exception that will be thrown by keys
     std::exception_ptr e;
-    auto onError = [&e]( std::exception_ptr exc) {
-        e = exc;
-    };
+    auto onError = [&e] ( std::exception_ptr exc ) { e = exc; };
 
+    // normally this would be run in its own thread
     keys.capture ( onEvent, onError );
+
+    if ( e != nullptr ) {
+        try {
+            std::rethrow_exception ( e );
+        } catch ( const hemiola::IoException& ex ) {
+            EXPECT_EQ ( ex.what(), std::string ( "No more data to read." ) );
+            EXPECT_EQ ( ex.code(), 42 );
+        } catch ( ... ) {
+            FAIL() << "Expected an IoException but got something else.";
+        }
+    } else {
+        FAIL() << "Expected an Exception but didn't get one.";
+    }
 }
